3c3409656a741bec97dafbcaa7c7d0b2da3efbb6
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-10-05 20:19:20 +0530

    feat: train model on custom data

---
+++ train.py
@@ -0,0 +1,62 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+
+import os
+import torch
+import data_setup, engine, model_builder, utils
+
+from torchvision import transforms
+
+# setup hyperparameters
+NUM_EPOCHS = 5
+BATCH_SIZE = 32
+HIDDEN_UNITS = 10
+LEARNING_RATE = 0.001
+
+# setup directories
+train_dir = "data/pizza_steak_sushi/train"
+test_dir = "data/pizza_steak_sushi/test"
+
+# setup target device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# create transforms
+data_transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+# create dataloaders with the help of data_setup.py
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE
+)
+
+# create model with the help of model_builder.py
+model = model_builder.TinyVGG(
+    input_shape=3,
+    hidden_units=HIDDEN_UNITS,
+    output_shape=len(class_names)
+).to(device)
+
+# set loss and optimizer
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# start training with help from engine.py
+engine.train(model=model,
+             train_dataloader=train_dataloader,
+             test_dataloader=test_dataloader,
+             loss_fn=loss_fn,
+             optimizer=optimizer,
+             epochs=NUM_EPOCHS,
+             device=device)
+
+# save the model with help from utils.py
+utils.save_model(model=model,
+                 target_dir="models",
+                 model_name="going_modular_model.pth")
+             
